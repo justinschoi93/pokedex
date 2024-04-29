429fb912ff2b44ede1abf0a46d578b0e20f38227
jOwl <jhome@js-MacBook-Pro-2.local>
2024-04-29 13:20:50 -0700

    new blueprints. searchBar will getSprite, getPokemonByName, getPokemonDescription, resfreshPokedex

--- client/src/components/Pokedex/Pokedex.jsx
+++ client/src/components/Pokedex/Pokedex.jsx
@@ -2,57 +2,70 @@ import './pokedex.css';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
-const pokedex = {
-    axios: axios.create({baseURL: 'https://pokeapi.co/api/v2/'}),
-    getSprites: async function (name) {
-        try {
-            const response = await this.axios.get(`pokemon/${name}`);
-            const sprites = response.data.sprites;
 
-            return sprites;
-        } catch (error) {
-            console.error(error);
-        }
-    }
-}
 
-function searchBarHandler(name) {
-    setPokemon(name);
-}
 
 const Pokedex = () => {
     const [pokemon, setPokemon] = useState('pikachu');
-    const [sprites, setSprites] = useState([]);
+    const [sprite, setSprite] = useState({});
+    const [searchForm, setSearchForm] = useState({});
 
+    
+    const pokedex = {
+        axios: axios.create({baseURL: 'https://pokeapi.co/api/v2/'}),
+        getSprite: async function (name) {
+            try {
+                const response = await this.axios.get(`pokemon/${name}`);
+                const sprites = response.data.sprites;
+                setSprite({front_default: sprites.front_default});
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
+    
+    
+    
     useEffect(() => {
-        let newSprites = [];
-
-        (async () => {
-            const response = await pokedex.getSprites('pikachu');
-
-            Object.entries(response).forEach( entry => {
-                newSprites.push({[entry[0]]: entry[1]});
-            })
-            
-            setSprites(newSprites);
+        (async () => {      // will setSprite
+            await pokedex.getSprite(pokemon);
         })();
     }, [pokemon])
+    
+
+    const handleInputChange = (e) => {
+        e.preventDefault;
+
+        setSearchForm(e.target.value);
+    }
+
+    const searchBarHandler = (e) => {
+        e.preventDefault();
+
+        setPokemon(e.target.value);
+    } 
+
+
     
     return (
         <div>
             <h1>Pokedex</h1>
             <div className="sprites pikachu-sprite">
-                {/* {console.log(Object.keys(sprites[0]), Object.values(sprites[0]))} */}
-                {sprites.map( sprite => {
-                    return (
-                        <div className="sprite-card">
-                            <img src={Object.values(sprite)} alt={Object.keys(sprite)} className={`${Object.keys(sprite)}-sprite`} />
-                            <p className="sprite-key">{Object.keys(sprite)}</p>
-                        </div>
-                    )
-                })}
-                
+                <div className="sprite-card" key={pokemon}>
+                    <img src={sprite} alt={"front_default"} className={`${pokemon}-sprite`} />
+                    <p className="sprite-key">{pokemon}</p>
+                </div>
             </div>
+            <form onSubmit={searchBarHandler}>
+                <input 
+                    type="text" 
+                    value={searchForm} 
+                    onChange={handleInputChange} 
+                    className="searchBar__input" 
+                    placeholder="Search for a Pokemon"
+                />
+                <button type="submit" className="searchBar__button">Search</button>
+            </form>
         </div>
     )
 }
